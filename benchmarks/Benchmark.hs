dbebf34fc92514b9f9ec5153a50008481e7268aa
Christopher Chalmers <c.chalmers@me.com>
2014-11-20 22:11:24 +0000

    Update benchmarks.

--- benchmarks/Benchmark.hs
+++ benchmarks/Benchmark.hs
@@ -5,6 +5,9 @@ import Data.Word
 import qualified System.Random as R
 import qualified System.Random.MWC as MWC
 import qualified System.Random.PCG as PCG
+import qualified System.Random.PCG.Fast as F
+import qualified System.Random.PCG.Single as S
+import qualified System.Random.PCG.Unique as U
 import qualified System.Random.Mersenne as M
 
 benchIO :: String -> IO a -> Benchmark
@@ -14,10 +17,22 @@ main :: IO ()
 main = do
   mwc <- MWC.create
   pcg <- PCG.create
+  pcgF <- F.create
+  pcgS <- S.create
+  pcgU <- U.create
   mtg <- M.newMTGen . Just =<< MWC.uniform mwc
   defaultMain
     [ bgroup "pcg"
       [ benchIO "Word32" (PCG.uniform pcg :: IO Word32)
+      ]
+    , bgroup "pcg-fast"
+      [ benchIO "Word32" (F.uniform pcgF :: IO Word32)
+      ]
+    , bgroup "pcg-single"
+      [ benchIO "Word32" (S.uniform pcgS :: IO Word32)
+      ]
+    , bgroup "pcg-unique"
+      [ benchIO "Word32" (U.uniform pcgU :: IO Word32)
       ]
     , bgroup "mwc"
       [ benchIO "Word32" (MWC.uniform mwc :: IO Word32)
@@ -26,8 +41,8 @@ main = do
       ]
     -- , bgroup "random"
     --   [ benchIO "Word32" (R.randomIO >>= evaluate :: IO Word32)
-    --   , bench "Double" (R.randomIO >>= evaluate :: IO Double)
-    --   , bench "Int"    (R.randomIO >>= evaluate :: IO Int)
+    --   , benchIO "Double" (R.randomIO >>= evaluate :: IO Double)
+    --   , benchIO "Int"    (R.randomIO >>= evaluate :: IO Int)
     --   ]
     , bgroup "mersenne"
       [ benchIO "Word32" (M.random mtg :: IO Word32)
