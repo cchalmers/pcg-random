d950a785d5671f85423552971ad91705ed7a41c6
Christopher Chalmers <c.chalmers@me.com>
2014-12-14 07:55:23 +0000

    Include c outputs in doctest.

--- test/doctest.hs
+++ test/doctest.hs
@@ -1,3 +1,8 @@
 import Test.DocTest
 
-main = doctest ["src/System/Random/PCG.hs"]
+main = doctest
+  [ "src/System/Random/PCG.hs"
+  , "dist/build/c/pcg-advance-64.o"
+  , "dist/build/c/pcg-output-64.o"
+  , "dist/build/c/pcg-rngs-64.o"
+  ]
