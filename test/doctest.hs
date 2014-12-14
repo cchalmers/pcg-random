6f31a564f9e809815ff95fc836ffb6adc653a632
Christopher Chalmers <c.chalmers@me.com>
2014-12-14 16:00:17 +0000

    Typo.

--- test/doctest.hs
+++ test/doctest.hs
@@ -2,7 +2,7 @@ import Test.DocTest
 
 main = doctest
   [ "src/System/Random/PCG.hs"
-  [ "src/System/Random/PCG/Class.hs"
+  , "src/System/Random/PCG/Class.hs"
   , "dist/build/c/pcg-advance-64.o"
   , "dist/build/c/pcg-output-64.o"
   , "dist/build/c/pcg-rngs-64.o"
