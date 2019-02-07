a16fecb1fe519c98b7380890415f9b0bc5c2769c
Kirill Zaborsky <qrilka@gmail.com>
2019-02-07 13:08:47 +0300

    Fix tests

--- test/doctest.hs
+++ test/doctest.hs
@@ -1,12 +1,17 @@
+import System.Environment (getEnvironment)
 import Test.DocTest
 
-main = doctest
-  [ "src/System/Random/PCG.hs"
-  , "src/System/Random/PCG/Class.hs"
-  , "src/System/Random/PCG/Pure.hs"
-  , "src/System/Random/PCG/Fast.hs"
-  , "src/System/Random/PCG/Single.hs"
-  , "dist/build/c/pcg-advance-64.o"
-  , "dist/build/c/pcg-output-64.o"
-  , "dist/build/c/pcg-rngs-64.o"
-  ]
+main = do
+  env <- getEnvironment
+  let dist = case lookup "HASKELL_DIST_DIR" env of
+               Nothing -> "dist"
+               Just x -> x
+  doctest [ "src/System/Random/PCG.hs"
+          , "src/System/Random/PCG/Class.hs"
+          , "src/System/Random/PCG/Pure.hs"
+          , "src/System/Random/PCG/Fast.hs"
+          , "src/System/Random/PCG/Single.hs"
+          , dist ++ "/build/c/pcg-advance-64.o"
+          , dist ++ "/build/c/pcg-output-64.o"
+          , dist ++ "/build/c/pcg-rngs-64.o"
+          ]
